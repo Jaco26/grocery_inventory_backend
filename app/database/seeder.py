60740c8d276ec5d91a0dcfb8fa01bfd59c1bed74
Jacob Albright <jacob.albright23@gmail.com>
2020-03-16 19:00:35 -0500

    wrote script to seed fresh database. registered as flask app cli command

--- app/database/seeder.py
+++ app/database/seeder.py
@@ -1,1 +1,33 @@
 # write sqlalchemy script to seed fresh database with necessary entries
+
+import click
+from flask.cli import with_appcontext
+
+from app.database.db import db
+from app.database.models import (
+  PackagingState,
+  PackagingKind,
+)
+
+
+@click.command('seed-db')
+@with_appcontext
+def seed_db():
+  db.session.add_all([
+    PackagingKind(name="N/A", uniform_name="n/a"),
+    PackagingKind(name="Prepackaged Glass Jar", uniform_name="prepackaged_glass_jar"),
+    PackagingKind(name="Plastic Produce Bag", uniform_name="plastic_produce_bag"),
+    PackagingKind(name="Mason Jar", uniform_name="mason_jar"),
+    PackagingKind(name="Prepackaged Cardbord Box", uniform_name="prepackaged_cardboard_box"),
+  ])
+
+  db.session.add_all([
+    PackagingState(name="N/A", uniform_name="n/a"),
+    PackagingState(name="Unopened", uniform_name="unopened"),
+    PackagingState(name="Disgarded", uniform_name="disgarded"),
+    PackagingState(name="Opened", uniform_name="opened"),
+  ])
+
+  db.session.commit()
+
+
--- app/extensions/db.py
+++ app/extensions/db.py
@@ -3,6 +3,8 @@
 def init_app(app):
   from flask_migrate import Migrate
   from app.database.db import db
+  from app.database.seeder import seed_db
   
+  app.cli.add_command(seed_db)
   db.init_app(app)
   Migrate(app, db)
